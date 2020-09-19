65281f10d1beeb3a4cedca51d8566fab0203a8d4
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-09-19 23:10:23 +0530

    Update variables-auth.tf

--- variables-auth.tf
+++ variables-auth.tf
@@ -1,1 +1,36 @@
 
+##################
+# Authentication #
+##################
+
+# azure authentication variables
+variable "azure-subscription-id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "azure-client-id" {
+  type        = string
+  description = "Azure Client ID"
+}
+
+variable "azure-client-secret" {
+  type        = string
+  description = "Azure Client Secret"
+}
+
+variable "azure-tenant-id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+
+variable "spn_permissions" {
+  type = list
+  default = [
+    "Key Vault Administrator (preview)",
+    "Key Vault Reader (preview)",
+    "Key Vault Secrets User (preview)",
+    "Key Vault Secrets Officer (preview)",
+     ]
+}

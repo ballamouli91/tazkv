aea554c7fd00e4c7a73e61ba0321422e24fcca28
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-09-20 22:26:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ module "keyvault" {
 resource "null_resource" "enable-rbac" {
   provisioner "local-exec" {
     command     = "az keyvault update --name ${module.keyvault.name} -g ${module.resroucegroup.name}  --enable-rbac-authorization true"
-    interpreter = ["-Command"]
   }
 }
 

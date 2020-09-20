321f6192e0f4da8f38462a11a9fbd21058607718
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-09-20 20:16:25 +0530

    Update main.tf

--- logs/main.tf
+++ logs/main.tf
@@ -13,8 +13,8 @@ resource "azurerm_monitor_diagnostic_setting" "vault_monitor" {
   name               = local.monitor_name
   target_resource_id = var.target_resource_id
    # log destinations. Currently, we have only implemented log analytics.
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.vault_loganalytics.id
-  // azurerm_log_analytics_workspace.vault_loganalytics.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.vault_loganalytics.workspace_id
+  # azurerm_log_analytics_workspace.vault_loganalytics.id
    # log destinations. to retain logs.
   // storage_account_id = local.vault_name
   // One of eventhub_authorization_rule_id, log_analytics_workspace_id and storage_account_id must be specified.

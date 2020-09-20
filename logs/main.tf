a851cd1efd3e2f33172e71a829f6dc7696a2d6f3
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-09-20 21:35:37 +0530

    Update main.tf

--- logs/main.tf
+++ logs/main.tf
@@ -1,18 +1,9 @@
-resource "azurerm_log_analytics_workspace" "vault_loganalytics" {
-  name                = local.loganalytics_name
-  location            = var.location
-  resource_group_name = local.resource_group_name
-  sku                 = "PerGB2018"
-  ## default range between 30 and 730
-  retention_in_days   = 30  
-  tags = var.default_tags
-}
 
 resource "azurerm_monitor_diagnostic_setting" "vault_monitor" {
   name               = local.monitor_name
   target_resource_id = var.target_resource_id
    # log destinations. Currently, we have only implemented log analytics.
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.vault_loganalytics.workspace_id
+  log_analytics_workspace_id = var.var.log_analytics_workspace_id
   # azurerm_log_analytics_workspace.vault_loganalytics.id
    # log destinations. to retain logs.
   // storage_account_id = local.vault_name
@@ -33,7 +24,4 @@ resource "azurerm_monitor_diagnostic_setting" "vault_monitor" {
       enabled = true
     }
   }
-  depends_on = [
-    azurerm_log_analytics_workspace.vault_loganalytics,
-  ]
 }
